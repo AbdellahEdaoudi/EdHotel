c4d45cd8036b1c93cc1fc47c1d0f4763e44e460b
abdellah <abdellahedaoudi80@gmail.com>
2024-05-15 03:14:35 +0100

    SuspenseBoundary

--- app/Checkout/page.jsx
+++ app/Checkout/page.jsx
@@ -1,25 +1,27 @@
-"use client"
-import React from 'react'
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from '../Pages/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
+import { SuspenseBoundary } from 'react ';
+
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
-
 function page() {
-    const SearchParams = useSearchParams()
+    const SearchParams = useSearchParams();
     const options = {
-        mode :"payment",
-        currency:"usd",
+        mode: "payment",
+        currency: "usd",
         amount: Number(SearchParams.get('amount')) * 100
-    }
-  return (
-    <Elements stripe={stripePromise} options={options}>
-        
-      <CheckoutForm amount={Number(SearchParams.get('amount'))} />
-    </Elements>
-  )
+    };
+
+    return (
+        <SuspenseBoundary fallback={<div>Loading...</div>}>
+            <Elements stripe={stripePromise} options={options}>
+                <CheckoutForm amount={Number(SearchParams.get('amount'))} />
+            </Elements>
+        </SuspenseBoundary>
+    );
 }
 
-export default page
+export default page;
