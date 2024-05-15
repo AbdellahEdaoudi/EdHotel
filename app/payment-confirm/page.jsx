9d8fb604b4a715c0cb4482b38fca86493a81340f
abdellah <abdellahedaoudi80@gmail.com>
2024-05-15 05:13:53 +0100

    Payment With Stripe

--- app/payment-confirm/page.jsx
+++ app/payment-confirm/page.jsx
@@ -11,9 +11,11 @@ function page() {
 				height={130}
 			/>
 			<h2 className='text-[24px] text-black'>Payment Successful !</h2>
-			<h2 className='text-[17px] text-center mt-6 text-gray-500'>We sent an email with your
-				order confirmation
-				along with Digital Content</h2>
+			<h2 className='text-[17px]  text-center  mt-6 text-gray-500'>
+			Your booking confirmation email has been sent,
+			 and it includes all the details about your reservation.
+			 Thank you for choosing our hotel app
+			</h2>
 			<Link
 				href="/HomePage"
 				className='p-2 mt-6 text-white rounded-md bg-sky-500'>
