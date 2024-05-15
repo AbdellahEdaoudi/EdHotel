bdcab6f899812dffd2ccb75097f5ecdf1aa1a8a4
abdellah <abdellahedaoudi80@gmail.com>
2024-05-15 03:31:39 +0100

    Suspense

--- app/Checkout/page.jsx
+++ app/Checkout/page.jsx
@@ -1,25 +1,27 @@
-"use client"
-import React from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from '../Pages/CheckoutForm';
-import { useSearchParams } from 'next/navigation';
+"use client";
+import React, { Suspense } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../Pages/CheckoutForm";
+import { useSearchParams } from "next/navigation";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
-function Page() { 
-    const SearchParams = useSearchParams();
-    const options = {
-        mode: "payment",
-        currency: "usd",
-        amount: Number(SearchParams.get('amount')) * 100
-    };
+function Page() {
+  const SearchParams = useSearchParams();
+  const options = {
+    mode: "payment",
+    currency: "usd",
+    amount: Number(SearchParams.get("amount")) * 100,
+  };
 
-    return (
-            <Elements stripe={stripePromise} options={options}>
-                <CheckoutForm amount={Number(SearchParams.get('amount'))} />
-            </Elements>
-    );
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm amount={Number(SearchParams.get("amount"))} />
+      </Elements>
+    </Suspense>
+  );
 }
 
 export default Page;
