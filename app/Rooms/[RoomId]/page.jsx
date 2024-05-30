6b93953521de0b361655a897a3b1388e8e9b3cb5
abdellah <abdellahedaoudi80@gmail.com>
2024-05-30 02:35:46 +0100

    ADMIN

--- app/Rooms/[RoomId]/page.jsx
+++ app/Rooms/[RoomId]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
