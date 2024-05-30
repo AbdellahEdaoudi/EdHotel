f2feeee06e245cecdde8f41ddc3352da0524c480
abdellah <abdellahedaoudi80@gmail.com>
2024-05-30 02:43:23 +0100

    ADMIN v2

--- app/Pages/CheckoutForm.jsx
+++ app/Pages/CheckoutForm.jsx
@@ -39,7 +39,6 @@ const CheckoutForm = ({ amount }) => {
         data: { bookings: userBookings }
       });
       console.log(response.data);
-      window.location.reload();
     } catch (error) {
       console.error('Error deleting bookings:', error);
     }
